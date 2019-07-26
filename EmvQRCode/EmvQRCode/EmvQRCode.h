945180f810516ed58293df9c88a589c907ea950d
mineachem <minea.chem@gmail.com>
2019-07-26 11:11:18 +0700

    add  new folder

---
+++ EmvQRCode/EmvQRCode/EmvQRCode.h
@@ -0,0 +1,19 @@
+//
+//  EmvQRCode.h
+//  EmvQRCode
+//
+//  Created by User on 7/26/19.
+//  Copyright © 2019 Minea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmvQRCode.
+FOUNDATION_EXPORT double EmvQRCodeVersionNumber;
+
+//! Project version string for EmvQRCode.
+FOUNDATION_EXPORT const unsigned char EmvQRCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmvQRCode/PublicHeader.h>
+
+
