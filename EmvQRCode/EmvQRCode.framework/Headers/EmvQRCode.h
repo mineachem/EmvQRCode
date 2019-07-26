c1cb784768f74e0190935164caafa025e4153a3f
mineachem <minea.chem@gmail.com>
2019-07-26 11:15:02 +0700

    add framework

---
+++ EmvQRCode/EmvQRCode.framework/Headers/EmvQRCode.h
@@ -0,0 +1,19 @@
+//
+//  EmvQRCode.h
+//  EmvQRCode
+//
+//  Created by Admin on 5/26/19.
+//  Copyright © 2019 Idealink.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmvQRCode.
+FOUNDATION_EXPORT double EmvQRCodeVersionNumber;
+
+//! Project version string for EmvQRCode.
+FOUNDATION_EXPORT const unsigned char EmvQRCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmvQRCode/PublicHeader.h>
+
+
